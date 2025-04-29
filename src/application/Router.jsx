4dd65a6ad71bdce0e54aeaeb0e821a4b00dce975
carocitta <carocitta@gmail.com>
2025-04-29 15:35:56 +0200

    added aboutpage jsx and css

--- src/application/Router.jsx
+++ src/application/Router.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import App from "../App";
 import Homepage from "../pages/Homepage/HomePage";
+import AboutPage from "../pages/AboutPage/AboutPage";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/sobre-nosotras" element={<AboutPage/>}>
+        </Route>
       </Routes>
     </BrowserRouter>
   );
