7b440033334dd161f9f5bfd505e3984d00e2f609
Karisha:) <krshmelendez@gmail.com>
2025-04-29 16:13:32 +0200

    added icons to footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,13 +1,16 @@
+import  "./footer.css";
+import facebookImg from "/img/facebook.svg?url"
 
 function Footer(){
     return(
         <footer className='footer-container'>
-            <div className="text-container">
-            <p>
-            © 2025 Commit-Me. Todos los derechos reservados. Plataforma para developers, coders y entusiastas del software en España. Descubre, comparte y apúntate a los mejores eventos tech cerca de ti.
-            </p>
+            <div>
+            <a href="https://coreui.io"> &copy; Commit-me </a>
+            <span className="text"> Todos los derechos reservados. Plataforma para developers, coders y entusiastas del software en España. Descubre, comparte y apúntate a los mejores eventos tech cerca de ti.</span>
             </div>
 
+            <img className='footerSocial' src={facebookImg} />
+            <p  >Facebook</p>
         </footer>
     )
 }
