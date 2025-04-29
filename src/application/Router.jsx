422d1ad5539497204cdd25dd59f82a8a471c3a7e
carocitta <carocitta@gmail.com>
2025-04-29 16:11:39 +0200

    added header css and jsx

--- src/application/Router.jsx
+++ src/application/Router.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router";
+import Header from "../components/Header";
 import App from "../App";
 import Homepage from "../pages/Homepage/HomePage";
 import AboutPage from "../pages/AboutPage/AboutPage";
@@ -6,10 +7,11 @@ import AboutPage from "../pages/AboutPage/AboutPage";
 function Router() {
   return (
     <BrowserRouter>
+    <Header />
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="/sobre-nosotras" element={<AboutPage/>}>
-        </Route>
+        <Route path="/sobre-nosotras" element={<AboutPage />} />
+
       </Routes>
     </BrowserRouter>
   );
