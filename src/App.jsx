0c5fea4ca19f7e19d41d6f374dd5133c2d43841e
carocitta <carocitta@gmail.com>
2025-04-29 10:52:48 +0200

    install bootstrap dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import './App.css'
+import Header from "./components/../components/Header/Header";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-    
+      <Header />
+      <h1>hola </h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
