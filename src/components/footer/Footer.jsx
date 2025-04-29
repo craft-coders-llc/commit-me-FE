0e45a3fd533f452b27543a96164063aebb1e8ba5
Karisha:) <krshmelendez@gmail.com>
2025-04-29 10:53:13 +0200

    changed footer css

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,9 +2,12 @@
 function Footer(){
     return(
         <footer className='footer-container'>
+            <div className="text-container">
             <p>
-                hola
+            © 2025 Commit-Me. Todos los derechos reservados. Plataforma para developers, coders y entusiastas del software en España. Descubre, comparte y apúntate a los mejores eventos tech cerca de ti.
             </p>
+            </div>
+
         </footer>
     )
 }
