550f6a42bf97306088b9bf0d9bb90d4b0d0eecc0
Marta Ibarra <martaa.ibarra@hotmail.com>
2025-04-30 14:31:40 +0200

    Add temporary route to EventCard component

--- src/application/Router.jsx
+++ src/application/Router.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 import App from "../App";
 import Homepage from "../pages/Homepage/HomePage";
+import EventCard from "../components/EventCard/EventCard";
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
+        <Route path="/pruebas" element={<EventCard />} />
       </Routes>
     </BrowserRouter>
   );
