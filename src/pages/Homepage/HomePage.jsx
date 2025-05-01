332ce374117eaf17018361a2738dd4a29cf8b997
Marta Ibarra <martaa.ibarra@hotmail.com>
2025-05-01 19:25:29 +0200

    Add mobile media query

--- src/pages/Homepage/HomePage.jsx
+++ src/pages/Homepage/HomePage.jsx
@@ -3,22 +3,24 @@ import Footer from "../../components/footer/Footer"
 function Homepage() {
   return (
     <>
-    <section className="main-section">
-      <div className="main-section_intro">
-        <div className="coso">
-          <h1 className="main-text">Eventos en vivo, sin importar tu stack</h1>
-          <button type="button" className="btn  btn-lg">
-            Quiénes somos
-          </button>
+      <section className="main-section">
+        <div className="main-section_container">
+          <div className="main-section_info">
+            <h1 className="main-text">
+              Eventos en vivo, sin importar tu stack
+            </h1>
+            <button type="button" className="btn  btn-lg">
+              Quiénes somos
+            </button>
+          </div>
+          <img
+            className="main-section_image"
+            src="/img/evento_4.jpg"
+            alt="imagen de evento tech"
+          />
         </div>
-        <img
-          className="main-section_image"
-          src="/img/evento_4.jpg"
-          alt="imagen de evento tech"
-        />
-      </div>
-    </section>
-    <Footer/>
+      </section>
+      <Footer />
     </>
   );
 }
