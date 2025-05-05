bdad4af787a5ba76b5418b7bcc69f61a67ffe28e
carocitta <carocitta@gmail.com>
2025-05-05 09:31:41 +0200

    create registration page

---
+++ src/pages/RegistrationPage/RegistrationPage.jsx
@@ -0,0 +1,11 @@
+import FormRegistration from "../../components/FormRegistration/FormRegistration";
+
+function App() {
+
+    return (<div className="App">
+        <FormRegistration />
+    </div>;
+    );
+}
+
+export default App;
