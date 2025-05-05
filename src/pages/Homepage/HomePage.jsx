c681cdbf865578753bcdf0f1e6e68f0cd2560ee2
Marta Ibarra <martaa.ibarra@hotmail.com>
2025-05-05 17:31:38 +0200

    Create page for form component to add events

--- src/pages/Homepage/HomePage.jsx
+++ src/pages/Homepage/HomePage.jsx
@@ -3,30 +3,27 @@ import Footer from "../../components/footer/Footer";
 import Button from "../../components/button/Button";
 function Homepage() {
   return (
-    <>
-      <section className="main-section">
-        <div className="main-section_container">
-          <div className="main-section_info">
-            <h1 className="main-section_text">
-              Eventos en vivo, sin importar tu stack
-            </h1>
+    <section className="main-section">
+      <div className="main-section_container">
+        <div className="main-section_info">
+          <h1 className="main-section_text">
+            Eventos en vivo, sin importar tu stack
+          </h1>
 
-            <Button
-              type="button"
-              className="btn  btn-lg"
-              text="Quiénes somos"
-              to={"/about"}
-            />
-          </div>
-          <img
-            className="main-section_image"
-            src="/img/evento_4.jpg"
-            alt="imagen de evento tech"
+          <Button
+            type="button"
+            className="btn  btn-lg"
+            text="Quiénes somos"
+            to={"/about"}
           />
         </div>
-      </section>
-      <Footer />
-    </>
+        <img
+          className="main-section_image"
+          src="/img/evento_4.jpg"
+          alt="imagen de evento tech"
+        />
+      </div>
+    </section>
   );
 }
 
