0b284841a5189623e4220179cb51cee4fa0fb5bd
Marta Ibarra <martaa.ibarra@hotmail.com>
2025-05-06 22:29:08 +0200

    Create hook to fetch from API

---
+++ src/services/getApiHook.js
@@ -0,0 +1,30 @@
+const headers = { "Content-Type": "application/json" };
+
+export const getUserById = async (id) => {
+  return fetch(`http://localhost:8080/api/users/${id}`)
+    .then((res) => res.json())
+    .then((data) => {
+      return data;
+    })
+    .catch((err) => console.error(err));
+};
+
+export const getAllUsers = async () => {
+  return fetch("http://localhost:8080/api/v1/users")
+    .then((res) => res.json())
+    .then((data) => {
+      return data;
+    })
+    .catch((err) => console.error(err));
+};
+
+export const createUser = (body) => {
+  fetch("http://localhost:8080/api/v1/users", {
+    method: "POST",
+    headers: headers,
+    body: JSON.stringify(body),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.error(err));
+};
