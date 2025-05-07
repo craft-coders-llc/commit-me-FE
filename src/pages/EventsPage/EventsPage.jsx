97ccf2bf728f88bee1e1000da7c2134bc61f53dc
carocitta <carocitta@gmail.com>
2025-05-07 18:19:26 +0200

    added responsive mode

--- src/pages/EventsPage/EventsPage.jsx
+++ src/pages/EventsPage/EventsPage.jsx
@@ -2,10 +2,12 @@ import Footer from "../../components/footer/Footer";
 import EventsList from "../../components/EventsList/EventsList";
 import Button from "../../components/button/Button";
 import "../EventsPage/EventsPage.css";
+import FilterEvents from "../../components/FilterEvents/FilterEvents";
 
 function EventsPage() {
   return (
     <section>
+      <FilterEvents/>
       <EventsList />
       <Button type="button" className="btn btn-lg next-btn" text="Siguiente" />
 
