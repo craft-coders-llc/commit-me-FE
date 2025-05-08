c2a75f1c28da681f5245a0fd4b1235e7f1ddf252
Marta Ibarra <martaa.ibarra@hotmail.com>
2025-05-08 10:19:11 +0200

    Add filter component to events page

--- src/pages/EventsPage/EventsPage.jsx
+++ src/pages/EventsPage/EventsPage.jsx
@@ -7,6 +7,7 @@ import FilterEvents from "../../components/FilterEvents/FilterEvents";
 function EventsPage() {
   return (
     <section>
+      <FilterEvents />
       <EventsList />
       <Button
         type="button"
